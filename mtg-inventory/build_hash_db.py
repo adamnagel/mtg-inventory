ea6f46608841f0dc9b4e792d72d6353d4b813711
Adam Nagel <adam.nagel+git@gmail.com>
2018-12-31 17:29:49 -0600

    hash-building script maturation

--- mtg-inventory/build_hash_db.py
+++ mtg-inventory/build_hash_db.py
@@ -2,8 +2,11 @@
 
 from os.path import dirname, join, exists
 import json
-from subprocess import call, check_output
+from subprocess import check_output
 import re
+from os import walk
+from multiprocessing import Pool
+from copy import copy
 
 re_phash_start = '\s*Channel perceptual hash: sRGB'
 rec_phash_start = re.compile(re_phash_start)
@@ -39,7 +42,7 @@ def HashImg(path_img):
         path_img
     ])
 
-    ph = {}
+    data = {}
     active = False
     channel_dict = {}
 
@@ -58,16 +61,10 @@ def HashImg(path_img):
             num_channel = re.search('(?<=Channel )[0-9]{1}', line).group(0)
             channel = 'Channel {}'.format(num_channel)
 
-            ph[channel] = {}
-            channel_dict = ph[channel]
+            data[channel] = {}
+            channel_dict = data[channel]
 
         if rec_ph_pair.match(line):
-            # search = re.search('(?<=PH)[0-9]{1}: .*, .*', line)
-            #
-            # num_ph = search.group(0)
-            # ph1 = search.group(1)
-            # ph2 = search.group(2)
-
             trimline = line.strip()
             ph_, nums = trimline.split(':')
 
@@ -76,8 +73,63 @@ def HashImg(path_img):
             channel_dict[ph_] = [float(num1), float(num2)]
 
     # return result
-    print (json.dumps(ph, indent=2))
+    # print (json.dumps(data, indent=2))
+    return data
 
 
-data = HashImg(join(path_image_db_root, 'F', 'Forest', '3a11f9c9-267a-4913-97f7-78c372fe211e.jpg'))
-print (data)
+def HashImgWrap(item):
+    rtn = copy(item)
+    rtn['data'] = HashImg(item['_abspath'])
+    return rtn
+
+
+# What do we want to do?
+# We want to open the existing Hash DB and index the IDs.
+# Then we want to skip any images we've already indexed.
+
+with open(path_hash_db) as f:
+    hash_db = json.load(f)
+
+ids_completed = []
+for i in hash_db:
+    ids_completed.append(i['_id'])
+
+max = 100000000
+files_db = []
+skipped = 0
+
+for root, dirs, files in walk(path_image_db_root, topdown=False):
+    if max == 0:
+        break
+
+    for name in files:
+        id = name.replace('.jpg', '')
+        if id in ids_completed:
+            skipped += 1
+            continue
+
+        if max == 0:
+            break
+
+        max -= 1
+
+        abspath = join(root, name)
+        relpath = abspath.replace(path_image_db_root, '')
+        files_db.append({
+            '_id': id,
+            '_file': relpath,
+            '_abspath': abspath
+        })
+
+print ('Skipped {} already indexed'.format(skipped))
+print ('Indexing {} card images'.format(len(files_db)))
+
+p = Pool()
+r = p.map(HashImgWrap, files_db)
+
+combined_db = []
+combined_db.extend(hash_db)
+combined_db.extend(r)
+
+with open(path_hash_db, 'w') as f:
+    json.dump(combined_db, f, indent=2, sort_keys=True)
