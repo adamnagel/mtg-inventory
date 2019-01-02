f2938757c1091b6f65ccca0173eaf887feb39c91
Adam Nagel <adam.nagel+git@gmail.com>
2019-01-01 20:25:52 -0600

    CardMatcher experimentation and more test cards

--- mtg-inventory/CardMatcher.py
+++ mtg-inventory/CardMatcher.py
@@ -19,33 +19,47 @@ path_hash_db = join(path_db_root, 'hash_db.json')
 
 class CardMatcher(object):
     def __init__(self, path_hash_db):
+
+        start = time()
         with open(path_hash_db, encoding="utf8") as f:
             hash_db = json.load(f)
+        duration = time() - start
+        print('loaded db in {:.4g}ms'.format(duration * 1000))
+        # self.hash_db = hash_db
 
         # Parse Database
         l_coefficients = []
         l_ids = []
         set_lengths = set()
         d_ids = dict()
-        for entry in hash_db:
-            # coeffs = self.DataDictToVector(entry['data'])
-            coeffs = []
 
-            for k in entry['data'].keys():
-                for k2, v2 in entry['data'][k].items():
-                    coeffs.extend(v2)
+        start = time()
+        for entry in hash_db:
+            if len(entry['data'].keys()) != 3:
+                print('wrong coeffs for {}'.format(entry['_file']))
+                continue
+
+            coeffs = self.DataDictToVector(entry['data'])
+
+            len_coeffs = len(coeffs)
+            # if len_coeffs != 35:
+            #     print('wrong length ({}) for {}'.format(len_coeffs, entry['_file']))
+            #     continue
 
             l_ids.append(entry['_id'])
             l_coefficients.append(coeffs)
-            set_lengths.add(len(coeffs))
+            set_lengths.add(len_coeffs)
 
             d_ids[entry['_id']] = {
-                '_filepath': entry['_file']
+                '_file': entry['_file']
             }
 
         print('lengths: {}'.format(set_lengths))
         np_coefficients = np.array(l_coefficients)
         print('shape np_coefficients: {}'.format(np_coefficients.shape))
+
+        duration = time() - start
+        print('transformed db in {:.4g}ms'.format(duration * 1000))
 
         self.np_coefficients = np_coefficients
         self.d_ids = d_ids
@@ -55,26 +69,59 @@ class CardMatcher(object):
     def DataDictToVector(d):
         coeffs = []
 
-        for k in d.keys():
-            for k2, v2 in d[k].items():
-                coeffs.extend(v2)
+        # Channel 0: Red, Hue
+        # Channel 1: Green, Chroma
+        # Channel 2: Blue, Luma
+
+        # style = 'Luma'
+        style = 'NoHue'
+
+        if style is 'NoHue':
+            for k, v in d['Channel 0'].items():
+                coeffs.append(v[0])
+            for k, v in d['Channel 1'].items():
+                coeffs.extend(v)
+            for k, v in d['Channel 2'].items():
+                coeffs.extend(v)
+
+        elif style is 'Luma':
+            for k, v in d['Channel 2'].items():
+                coeffs.append(v[1])
+
+        elif style is 'Full':
+            for k in d.keys():
+                for k2, v2 in d[k].items():
+                    coeffs.extend(v2)
 
         return coeffs
 
-    def MatchCardCoeffs(self, np_coeffs):
-        print('start comparison')
+    def MatchCardCoeffs(self, np_card_coeffs):
+        # print('start comparison')
 
         start = time()
-        np_sums = np.sum((np_coeffs - self.np_coefficients) ** 2, axis=1)
+        np_sums = np.sum((np_card_coeffs - self.np_coefficients) ** 2, axis=1)
         end = time()
 
-        print(np_sums)
-        print('took {:.4g}ms'.format((end - start) * 1000))
+        # print(np_sums)
+        # print('took {:.4g}ms'.format((end - start) * 1000))
 
         idx = np.argmin(np_sums)
         print('best match: {} for {}'.format(np_sums[idx], self.d_ids[self.l_ids[idx]]))
         # print(np_coefficients[idx])
         # print(np_testimage)
+
+        # What's the match with Feast of Dreams?
+        ids_fod = set()
+        for k, v in self.d_ids.items():
+            if 'Feast' in v['_file'] and 'Dreams' in v['_file']:
+                ids_fod.add(k)
+        # print(ids_fod)
+
+        for id_fod in ids_fod:
+            idx_fod = self.l_ids.index(id_fod)
+            np_fod_coeffs = self.np_coefficients[idx_fod]
+
+            print(np.sum((np_card_coeffs - np_fod_coeffs) ** 2))
 
     def MatchCardImg(self, path_card):
         hash_testimage = self.DataDictToVector(HashImg(path_card))
@@ -85,5 +132,15 @@ class CardMatcher(object):
 
 cm = CardMatcher(path_hash_db)
 
-path_testimage = join(dirname(__file__), 'testdata', 'feast_of_dreams_upright.jpg')
+path_testimage = join(dirname(__file__), 'testdata', 'feast_of_dreams.jpg')
+cm.MatchCardImg(path_testimage)
+path_testimage = join(dirname(__file__), 'testdata', 'feast_of_dreams copy.jpg')
+cm.MatchCardImg(path_testimage)
+path_testimage = join(dirname(__file__), 'testdata', 'feast_of_dreams_poor.jpg')
+cm.MatchCardImg(path_testimage)
+path_testimage = join(dirname(__file__), 'testdata', 'jou-69-feast-of-dreams.jpg')
+cm.MatchCardImg(path_testimage)
+path_testimage = join(dirname(__file__), 'testdata', 'feast_of_dreams crop.jpg')
+cm.MatchCardImg(path_testimage)
+path_testimage = join(dirname(__file__), 'testdata', 'jou-69-feast-of-dreams-480.jpg')
 cm.MatchCardImg(path_testimage)
